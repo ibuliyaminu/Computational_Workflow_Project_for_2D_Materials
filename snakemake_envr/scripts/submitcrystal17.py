de6afada8304fd0826daf5711b38ab8df4d3fb84
Ismail A. Buliyaminu <buliyami@msu.edu>
2023-12-02 22:37:18 -0500

    Working on the rule2 which submit the job on HPCC. The d12 file will now become the input file and the output file also drop on the same directory which is required for the future calculations.

---
+++ snakemake_envr/scripts/submitcrystal17.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Script to submit Crystal17 jobs for D12 files in the current working directory."""
+
+import os
+import sys
+import math
+import re
+import linecache
+import shutil
+import itertools
+
+def main():
+    """Main function to execute Crystal17 jobs for D12 files."""
+    # Set the data_folder to the working directory
+    data_folder = "/mnt/home/buliyami/Computational_Workflow_Project_for_2D_Materials/snakemake_envr/result1/"  # Change This Directory to d12 Directory
+
+    data_files = os.listdir(data_folder)
+
+    # Loop through each file in the data_folder
+    for file_name in data_files:
+        # Check if the file is a D12 file
+        if ".d12" in file_name:
+            # Extract the submit_name from the file name
+            submit_name = file_name.split(".d12")[0]
+
+            # Run a system command to submit a Crystal17 job with the specified submit_name and 100 steps
+            os.system(data_folder + "/submitcrystal17.sh " + submit_name + " 100")
+
+if __name__ == "__main__":
+    main()
